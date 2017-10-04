da73133207f8fbed33bf302334170beaad043dcd
student <student@n-game.local>
2017-10-04 19:26:49 +0900

    first commit!

---
+++ Assets/TangoSDK/Examples/SimpleAugmentedReality/Scripts/MoveScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+	GameObject script;
+
+	// Use this for initialization
+	void Start () {
+		script = GameObject.Find ("Pin");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 TargetPosition;
+		MoveScript ms = script.GetComponent<MoveScript> ();
+		TargetPosition.Position = ms.Pos;
+	}
+}
---
+++ Assets/TangoSDK/Examples/SimpleAugmentedReality/Scripts/TestScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tango;
+
+public class TestScript : MonoBehaviour {
+    public TangoPointCloud pointCloud;
+	public Vector3 pos;
+	public Plane plane;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.touchCount > 0)
+        {
+            Touch t = Input.GetTouch(0);
+            TouchPhase p = t.phase;
+            if(p == TouchPhase.Began)
+            {
+                StartCoroutine(FindPlane(t.position));
+            }
+        }
+
+	}
+
+    private IEnumerator FindPlane(Vector2 touchPosition)
+    {
+        Camera cam = Camera.main;
+
+        if(!pointCloud.FindPlane(cam,touchPosition,out pos,out plane))
+        {
+            yield break;
+        }
+        transform.position = pos;
+        transform.forward = new Vector3(
+            cam.transform.position.x - transform.position.x,
+            0,
+            cam.transform.position.z - transform.position.z).normalized;
+		
+    }
+}
